9ceee0abe4c0737463ffde5278ee1340a4201ba8
kilkelly <frank.kilkelly@gmail.com>
2024-12-10 21:31:37 +0000

    Remove unnecessary files

--- src/routes/balance/+server.js
+++
@@ -1,8 +0,0 @@
-import nanogptjs from 'nanogptjs'
-
-export async function POST({ request }) {
-  let { apiKey } = await request.json()
-  const nanogpt = nanogptjs({ apiKey })
-  let balance = await nanogpt.balance()
-  return new Response(balance)
-}
